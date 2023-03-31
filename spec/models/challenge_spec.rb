4db8943a443ca83978111925281d9a0858871ff7
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-31 13:39:12 -0600

    Resolve 6 failing tests within models spec files

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Challenge, type: :model do
   describe "validations" do
     it {should validate_presence_of :theme}
     it {should validate_presence_of :project_budget}
-    
   end
 
   describe "relationships" do
