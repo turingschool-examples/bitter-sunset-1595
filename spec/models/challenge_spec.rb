4f4c30f9e8be115cf08a9c087418e73b0a3458b1
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-31 13:31:09 -0600

    Initial Commit

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Challenge, type: :model do
   describe "validations" do
     it {should validate_presence_of :theme}
     it {should validate_presence_of :project_budget}
+    
   end
 
   describe "relationships" do
