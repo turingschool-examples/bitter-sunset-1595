694cf3b1ab41f90d3caf56ef4d706c1b93c20377
margdaly <margaret.daly@gmail.com>
2023-03-31 16:43:06 -0600

    Time's up

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @contestants = Project.joins(:contestant_projects).where("project_id = #{params[:id]}").select(:name)
   end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe 'Project Show Page' do
   it "displays project's name, material, and it's challenge's theme" do
     recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-   
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    
     visit "/projects/#{news_chic.id}"
 
     expect(page).to have_content(news_chic.name)
     expect(page).to have_content(news_chic.material)
     expect(page).to have_content(news_chic.challenge_theme)
+    expect(page).to_not have_content(boardfit.name)
+  end
+
+  it "displays the number of contestants on this project" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("Number of Contestants on #{news_chic.name}: #{news_chic.contestants_count}")
+    expect(page).to_not have_content("Number of Contestants on #{boardfit.name}: #{boardfit.contestants_count}")
   end
 end
