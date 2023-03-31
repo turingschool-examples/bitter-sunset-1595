202ce0a2f826e155ec381cacf896b1a1545e00dc
margdaly <margaret.daly@gmail.com>
2023-03-31 16:39:50 -0600

    Add #contestant_count to projects

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -17,6 +17,10 @@ RSpec.describe Project, type: :model do
     @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
     @news_chic = @recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
     @boardfit = @recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
   end
 
   describe 'instance methods' do
@@ -24,6 +28,12 @@ RSpec.describe Project, type: :model do
       it 'returns the shelter name for the given pet' do
         expect(@news_chic.challenge_theme).to eq(@recycled_material_challenge.theme)
         expect(@boardfit.challenge_theme).to eq(@recycled_material_challenge.theme)
+      end
+    end
+
+    describe '.contestants_count' do
+      it 'returns the count of contestants' do
+        expect(@news_chic.contestants_count).to eq(2)
       end
     end
   end
