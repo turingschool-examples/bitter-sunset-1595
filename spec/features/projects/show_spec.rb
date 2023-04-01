913392720babdaeecf7bd9c79209222a41b2dc2e
Reid Miller <reid.s.miller@gmail.com>
2023-04-01 11:51:34 -0600

    test: Add tests for extension 2

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe '/projects/:id', type: :feature do
 
       @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
       @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
       ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
       ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
@@ -31,6 +32,27 @@ RSpec.describe '/projects/:id', type: :feature do
       visit "projects/#{@upholstery_tux.id}"
       
       expect(page).to have_content("Average Contestant Experience: 10.0 years")
+    end
+
+    describe 'User Story Extension 2' do
+      it 'I see a form to add a contestant to this project' do
+        visit "projects/#{@upholstery_tux.id}"
+
+        expect(page).to have_content('Contestant ID')
+        expect(page).to have_field('contestant_id')
+        expect(page).to have_button('Add Contestant To Project')
+
+        fill_in 'contestant_id', with: "#{@erin.id}"
+        click_button 'Add Contestant To Project'
+
+        expect(current_path).to eq("projects/#{@upholstery_tux.id}")
+        expect(page).to have_content("Number of Contestants: 3")
+        
+        visit "/contestants"
+        expect(page).to have_content(@erin.name)
+        expect(@upholstery_tux.name).to appear_after(@erin.name)
+        save_and_open_page
+      end
     end
   end
 end
