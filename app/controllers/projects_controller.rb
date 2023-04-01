2c334dc4be7cd6e2231bbc59d2a40cdc0285e3cd
Reid Miller <reid.s.miller@gmail.com>
2023-04-01 11:24:07 -0600

    test: Add feature and model test for user story 3

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge = @project.challenge
+    @count = @project.count_contestants
   end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe '/projects/:id', type: :feature do
     before(:each) do
       @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
       @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
+
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
     end
 
     it 'I can see that projects name, material and the challenge theme' do
@@ -13,6 +19,12 @@ RSpec.describe '/projects/:id', type: :feature do
       expect(page).to have_content("Project Upholstery Tuxedo")
       expect(page).to have_content("Material: Couch")
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
+    end
+
+    it 'I see a count of the number of contestants on this project' do
+      visit "projects/#{@upholstery_tux.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
     end
   end
 end
