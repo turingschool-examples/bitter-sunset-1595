fe5a26c5a515ff0a3e90f38eca8369e37ed63eef
Reid Miller <reid.s.miller@gmail.com>
2023-04-01 11:15:48 -0600

    feat: Add user story 2 functionality

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe '/contestants', type: :feature do
   describe 'As a visitor, when I visit the contestants index page' do
     before(:each) do
-      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
 
-      @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @news_chic = @recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
 
-      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create!(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create!(contestant_id: @jay.id, project_id: @boardfit.id)
     end
 
     it 'I see a list of names of all the contestants and a list of the projects theyve been on' do
@@ -17,7 +19,7 @@ RSpec.describe '/contestants', type: :feature do
 
       expect(page).to have_content('Contestant Index Page')
       expect(page).to have_content('Jay McCarroll')
-      expect(page).to have_content('Projects: News Chic')
+      expect(page).to have_content('Projects: News Chic, Boardfit')
     end
   end
 end
