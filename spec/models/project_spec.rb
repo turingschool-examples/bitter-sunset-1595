dc0caf22655f821ae891b169e41f3785260478ef
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-02 17:28:38 -0600

    Feat: Add route for show page and fix white space

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe Project, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
@@ -12,4 +11,6 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  
 end
