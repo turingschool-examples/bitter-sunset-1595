7d4beffba7b7da8f4fcce601870c20a4fca607ea
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-02 14:28:52 -0600

    Feat: add code for passing us1 tests

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+  def show
+    @projects = Project.all
+  end
+end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -9,18 +9,12 @@ RSpec.describe 'Project Show Page' do
   let!(:lit_fit) { furniture_challenge.projects.create!(name: "Litfit", material: "Lamp") }
   
   describe 'User Story 1' do
-    it 'when I visit a project show page, I see that projects name and material' do
+    it 'when I visit a project show page, I see that projects name, material, and challenge theme' do
       visit "/projects/#{news_chic.id}"
 
       expect(page).to have_content(news_chic.name)
-      expect(page).to have_content(news_chic.material)
+      expect(page).to have_content("Material: #{news_chic.material}")
+      expect(page).to have_content("Challenge Theme: #{news_chic.challenge.theme}")
     end
   end
-#   As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
 end
