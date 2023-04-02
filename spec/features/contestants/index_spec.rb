ee3ca70535548633c60a52533324b2fb24194f9b
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-02 14:36:26 -0600

    Test: add first test for us2

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -1,6 +1,4 @@
-require 'rails_helper'
-
-RSpec.describe '/projects/:id, Project Show Page' do
+RSpec.describe '/contestants, Contestant Index Page' do
   let!(:recycled_material_challenge) { Challenge.create!(theme: "Recycled Material", project_budget: 1000) }
   let!(:furniture_challenge) { Challenge.create(theme: "Apartment Furnishings", project_budget: 1000) }
   let!(:news_chic) { recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper") }
@@ -11,14 +9,27 @@ RSpec.describe '/projects/:id, Project Show Page' do
   let!(:gretchen) { Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) }
   let!(:kentaro) { Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8) }
   let!(:erin) { Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15) }
-  
-  describe 'User Story 1' do
-    it 'when I visit a project show page, I see that projects name, material, and challenge theme' do
-      visit "/projects/#{news_chic.id}"
 
-      expect(page).to have_content(news_chic.name)
-      expect(page).to have_content("Material: #{news_chic.material}")
-      expect(page).to have_content("Challenge Theme: #{news_chic.challenge.theme}")
+  describe 'User Story 2' do
+    it 'when I visit the contestants index page, I see a list of names of all the contestants and a list of projects ' do
+      visit "/contestants"
+
+      expect(page).to have_content(jay.name)
+      expect(page).to have_content(gretchen.name)
+      expect(page).to have_content(kentaro.name)
+      expect(page).to have_content(erin.name)
     end
   end
+  # User Story 2 of 3
+
+# As a visitor,
+# When I visit the contestants index page ("/contestants")
+# I see a list of names of all the contestants
+# And under each contestants name I see a list of the projects (names) that they've been on
+
+# (e.g.   Kentaro Kameyama
+#         Projects: Litfit, Rug Tuxedo
+
+#         Jay McCarroll
+#         Projects: LeatherFeather)
 end
