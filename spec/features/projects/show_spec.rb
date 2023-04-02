d104a57739ce1d3de31b64f80bb22eefa1852d24
Amy Spears <amspears007@gmail.com>
2023-04-02 12:06:31 -0600

    Add tests to projects show_spec

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Project Show', type: :feature do
+  recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
   news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
   boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
   
   describe 'User Story 1: As a visitor, when I visit ("/projects/:id")' do
-    it "I see I see the project's name and material" do
+    it "I see I see the project's name and material and theme of the challenge that this project belongs to." do
       news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
       boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
       visit "/projects/#{news_chic.id}"
 
-      
+      expect(page).to have_content("News Chic")
+      expect(page).to have_content("Newspaper")
+      expect(page).to have_content("Recycled Material")
+
+      expect(page).to_not have_content("Boardfit")
+
+
     end
   end
 
