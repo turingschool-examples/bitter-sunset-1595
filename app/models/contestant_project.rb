53163186fdc2dd66a4ef22d99f23f170443aaac4
Grace Joh <graceehjoh@gmail.com>
2023-04-03 18:02:14 -0500

    feat/test: add validations for Contestant Project

--- app/models/contestant_project.rb
+++ app/models/contestant_project.rb
@@ -1,4 +1,5 @@
 class ContestantProject < ApplicationRecord
+  validates_presence_of :contestant_id, :project_id
   belongs_to :contestant
   belongs_to :project
 end
--- spec/models/contestant_projects_spec.rb
+++ spec/models/contestant_projects_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 
 RSpec.describe ContestantProject, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :contestant_id}
+    it {should validate_presence_of :project_id}
+  end
+  
   describe "relationships" do
     it {should belong_to :contestant}
     it {should belong_to :project}
