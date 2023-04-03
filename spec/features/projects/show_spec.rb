3c581f035f9ce0d744c074c2bb3e428e3e15a21d
Young Koh <youngheon6@gmail.com>
2023-04-03 10:17:34 -0600

    feat: Test and implement user story 1

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'project show page' do
+  before(:each) do
+    @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @news_chic = @recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create!(name: "Boardfit", material: "Cardboard Boxes")
+  end
+
+  #User Story 1
+  describe "When i visit /projects/:id" do
+    it "displays the projects name and material and theme of challenge" do
+      visit "/projects/#{@news_chic.id}"
+
+      expect(page).to have_content("News Chic")
+      expect(page).to have_content("Material: Newspaper")
+      expect(page).to have_content("Challenge Theme: Recycled Material")
+
+      expect(page).to_not have_content("Boardfit")
+      expect(page).to_not have_content("Apartment Furnishings")
+    end
+  end
+end
