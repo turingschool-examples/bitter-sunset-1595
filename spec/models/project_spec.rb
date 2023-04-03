f6be7f5b67f881b5a23d1ed19afbf214e7134c69
Crow Rising <miscbyrd@gmail.com>
2023-04-03 17:22:18 -0600

    test: User story 3 for instance method of count contestants

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe 'instance methods' do 
+    before(:each) do
+      @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+      @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
+
+      @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    end
+
+    describe '#count_contestants' do
+      it 'returns number of contestants for project' do
+        expect(@upholstery_tux.count_contestants).to eq(2)
+      end
+    end
+  end
 end
