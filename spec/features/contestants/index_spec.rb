29d618229966e421b9480ef126322b696bb8dc96
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-03 12:21:57 -0400

    update tests for us2

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Contestants index page' do
   describe 'as a visitor' do
     it 'I see a list of names of all the contestants and their projects' do
-      @project_1 = Project.create!(name: "Litfit", material: "Lamp Shade")
-      @project_2 = Project.create!(name: "Upholstery Tuxedo", material: "Couch")
-      @project_3 = Project.create!(name: "LeatherFeather", material: "Bird")
+      @challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
+      @project_1 = @challenge.projects.create!(name: "Litfit", material: "Lamp Shade")
+      @project_2 = @challenge.projects.create!(name: "Rug Tuxedo", material: "Couch")
+      @project_3 = @challenge.projects.create!(name: "LeatherFeather", material: "Bird")
 
       @contestant_1 = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Denver", years_of_experience: 10)
       @contestant_2 = Contestant.create!(name: "Jay McCarroll", age: 36, hometown: "New York City", years_of_experience: 12)
@@ -16,9 +17,9 @@ RSpec.describe 'Contestants index page' do
       @contestant_2.projects << @project_3
 
       visit '/contestants'
-
+      save_and_open_page
       expect(page).to have_content(@contestant_1.name)
-      expect(page).to have_content("Projects: Litfit, Upholstery Tuxedo")
+      expect(page).to have_content("Projects: Litfit, Rug Tuxedo")
       expect(page).to have_content(@contestant_2.name)
       expect(page).to have_content("Projects: LeatherFeather")
 
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Project show page' do
       challenge = Challenge.create!(theme: "Derelicte", project_budget: 1000)
       project = challenge.projects.create!(name: "Zoolander", material: "Garbage")
       visit "/projects/#{project.id}"
-      save_and_open_page
+      
       expect(page).to have_content(project.name)
       expect(page).to have_content(project.material)
       expect(page).to have_content(project.challenge.theme)
