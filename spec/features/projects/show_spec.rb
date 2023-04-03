6b6973ea9d13c04207dae6c5f3a80f9dd62be9a5
Grace Joh <graceehjoh@gmail.com>
2023-04-03 16:58:32 -0500

    test: Project show page displays average years of contestant experience

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -4,20 +4,26 @@ RSpec.describe 'the Project Show page' do
   before(:each) do
     test_data
 
-    visit "/projects/#{@lit_fit.id}"
+    visit "/projects/#{@boardfit.id}"
   end
 
   describe 'User Story 1' do
     it 'displays the project name and material and the challenge theme' do
-      expect(page).to have_content(@lit_fit.name)
-      expect(page).to have_content("Material: #{@lit_fit.material}")
-      expect(page).to have_content("Challenge Theme: #{@lit_fit.challenge.theme}")
+      expect(page).to have_content(@boardfit.name)
+      expect(page).to have_content("Material: #{@boardfit.material}")
+      expect(page).to have_content("Challenge Theme: #{@boardfit.challenge.theme}")
     end
   end
 
   describe 'User Story 3' do
     it 'displays the number of contestants on each project' do
-      expect(page).to have_content("Number of Contestants: #{@lit_fit.contestants.size}")
+      expect(page).to have_content("Number of Contestants: #{@boardfit.contestants.size}")
+    end
+  end
+
+  describe 'Extension 1' do
+    it 'displays the number of contestants on each project' do
+      expect(page).to have_content("Average Contestant Experience: #{@boardfit.average_experience}")
     end
   end
 end
