beafcceb129168c6a375e2f726d1e9bc165420c8
Nick Sacco <nicksacco10@gmail.com>
2023-04-03 15:20:09 -0700

    add projects to contestants in tests

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe "Project Show Page", type: :feature do
     @contestant_2 = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
     @contestant_3 = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     @contestant_4 = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    @contestant_1.projects << @project
+    @contestant_2.projects << @project
+    @contestant_3.projects << @project
+    @contestant_4.projects << @project
   end
 
   it 'can show the projects name, material, and the challenge that it belongs to' do
