1a5fab7ea3cad3c3bd119dccb0ceacd71a937c0b
Nick Sacco <nicksacco10@gmail.com>
2023-04-03 15:31:28 -0700

    complete us3

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "Project Show Page", type: :feature do
   it 'can see the total number of contestants on a project' do
     visit "/projects/#{@project.id}"
     
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content("4")
   end
 end
