295d7dc47d0f2c098e683bb34201ada496a42d1a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-02 21:53:10 -0400

    feat: implement functionality to display contestants and their projects on contestant index page - User Story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/projects/:id', to: 'projects#show'
+
+  get '/contestants', to: 'contestants#index'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
