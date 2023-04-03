b53753e363c598ca462afcda47e555e5928b1cba
Crow Rising <miscbyrd@gmail.com>
2023-04-03 17:05:43 -0600

    feat: User story 2

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,8 +1,6 @@
 class ContestantsController < ApplicationController
 
   def index
-    # require 'pry'; binding.pry
-
     @contestants = Contestant.all
   end
 end
--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -12,17 +12,17 @@ RSpec.describe '/contestants' do
       @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
       @lit_fit = @furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
 
-      @ay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @jay = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
       @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
       @erin = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
-      # ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-      # ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-      # ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-      # ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-      # ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-      # ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
 
     end
 
@@ -33,6 +33,10 @@ RSpec.describe '/contestants' do
       expect(page).to have_content("Gretchen Jones")
       expect(page).to have_content(@kentaro.name)
       expect(page).to have_content(@erin.name)
+      expect(page).to have_content(@boardfit.name)
+      expect(page).to have_content(@upholstery_tux.name)
     end
+
+
   end
 end
