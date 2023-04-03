28707f5e36235b31016007d21be9ea6976345af3
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-02 22:09:30 -0400

    test: add test for project show spec to show the number of contestants on a project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,6 +10,17 @@ RSpec.describe 'the project show page' do
 
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
   end
 
   it 'should display a project name + materials and the challenge theme' do
@@ -22,5 +33,16 @@ RSpec.describe 'the project show page' do
     expect(page).to_not have_content(@news_chic.name)
     expect(page).to_not have_content(@news_chic.material)
     expect(page).to_not have_content(@news_chic.challenge.theme)
+  end
+
+  it 'should display a count of the number of contestants on the project' do
+    visit "/projects/#{@news_chic.id}"
+    expect(page).to have_content(2)
+
+    visit "/projects/#{@upholstery_tux.id}"
+    expect(page).to have_content(2)
+
+    visit "/projects/#{@boardfit.id}"
+    expect(page).to have_content(1)
   end
 end
