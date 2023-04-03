29c05d9b36430d37b7eab4909d8440f0b47f2497
Grace Joh <graceehjoh@gmail.com>
2023-04-03 16:57:01 -0500

    test: Project #average_experience

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      test_data
+    end
+
+    describe '#average_experience' do
+      it 'averages all contestants years of experience' do
+        expect(@news_chic.average_experience).to eq(12.5)
+        expect(@boardfit.average_experience).to eq(11.5)
+      end
+
+      it 'returns zero if no contestants are on the project' do
+        expect(@lit_fit.average_experience).to eq(0)
+      end
+    end
+  end
 end
