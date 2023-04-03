8f10df4a48ad543ae8d99202794e61e123c05afb
Crow Rising <miscbyrd@gmail.com>
2023-04-03 17:43:58 -0600

    Final Commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @challenge = @project.challenge
+    @contestant_count = @project.count_contestants
   end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe '/projects/:id' do
-   describe 'as a visitor visiting show page' do
+   describe 'as a visitor visiting projects show page' do
     before(:each) do
       @recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
       @furniture_challenge = Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000)
@@ -16,6 +16,14 @@ RSpec.describe '/projects/:id' do
       @gretchen = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
       @kentaro = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
       @erin = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+  
+      # ContestantProject.create!(contestant_id: @jay.id, project_id: @news_chic.id)
+      # ContestantProject.create!(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      # ContestantProject.create!(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      # ContestantProject.create!(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+      # ContestantProject.create!(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      # ContestantProject.create!(contestant_id: @erin.id, project_id: @boardfit.id)
+
     end
 
     it 'displays projects name and material' do
@@ -24,6 +32,18 @@ RSpec.describe '/projects/:id' do
       expect(page).to have_content(@news_chic.name)
       expect(page).to have_content('Newspaper')
       expect(page).to have_content(@news_chic.challenge_id)
+    end
+
+    it 'displays a count of contestants on the project' do
+     
+      visit "/projects/#{@news_chic.id}"
+      expect(page).to have_content(2)
+
+      visit "/projects/#{@upholstery_tux.id}"
+      expect(page).to have_content(2)
+
+      visit "/projects/#{@boardfit.id}"
+      expect(page).to have_content(1)
     end
   end
 end
