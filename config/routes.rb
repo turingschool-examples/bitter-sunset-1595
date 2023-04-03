7202c921fc7a73211a444838b8278847a1331e54
Thomas Hawley <thawley22@turing.edu>
2023-04-03 17:43:09 -0600

    feat: Add route for contestant_projects#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 get '/projects/:id', to: 'projects#show'
 get '/contestants', to: 'contestants#index'
+post '/contestant_projects', to: 'contestant_projects#create'
 end
