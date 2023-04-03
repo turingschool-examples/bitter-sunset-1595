bb36a7945eb948b76df908378f3c9be3ea8ba036
Thomas Hawley <thawley22@turing.edu>
2023-04-03 17:42:48 -0600

    test: Add test for adding contestant to a project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe '/projects/:id', type: :feature do
 
   let!(:jay) { Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13) }
   let!(:gretchen) { Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) }
+  let!(:erin) { Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15) }
 
   let!(:con_proj_1) { ContestantProject.create!(contestant_id: jay.id, project_id: news_chic.id) }
   let!(:con_proj_2) { ContestantProject.create!(contestant_id: gretchen.id, project_id: news_chic.id) }
@@ -30,6 +31,20 @@ RSpec.describe '/projects/:id', type: :feature do
       visit "/projects/#{news_chic.id}"
 
       expect(page).to have_content('Average Contestant Experience: 12.5 years')
+    end
+  end
+  
+  describe 'I see a form to add a contestant to this project' do
+    describe 'When I fill out the form with an existing contestants id and hit Add Contestant to project' do
+      it 'I am taken back to the show page and see the number of contestants has increased by 1' do
+        visit "/projects/#{news_chic.id}"
+
+        fill_in :contestant_id, with: erin.id
+        click_button('Add Contestant to Project')
+
+        expect(current_path).to eq("/projects/#{news_chic.id}")
+        expect(page).to have_content('Number of Contestants: 3')
+      end
     end
   end
 end
