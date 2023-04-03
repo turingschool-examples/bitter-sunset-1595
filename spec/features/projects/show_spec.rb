f5bce0ea6a6e36359872974361c40ea2aa3c43ff
Grace Joh <graceehjoh@gmail.com>
2023-04-03 18:03:26 -0500

    test: Extension 2 - add a contestant to a project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -26,4 +26,24 @@ RSpec.describe 'the Project Show page' do
       expect(page).to have_content("Average Contestant Experience: #{@boardfit.average_experience}")
     end
   end
+
+  describe 'Extension 2' do
+    it 'displays the form to add a contestant' do
+      expect(page).to have_content('Add a Contestant')
+      expect(page).to have_field('Contestant ID')
+      expect(page).to have_button('Add Contestant to Project')
+    end
+
+    it 'adds a contestant to the project' do
+      fill_in(:contestant_id, with: 1)
+      click_on('Add Contestant to Project')
+
+      expect(current_path).to eq("/projects/#{@boardfit.id}")
+      expect(page).to have_content('Number of Contestants: 3')
+
+      visit '/contestants'
+
+      expect(page).to have_content("Jay McCarroll\nProjects: News Chic, Boardfit")
+    end
+  end
 end
