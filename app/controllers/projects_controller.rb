dd92a13a2bf1273b229fad8ea11b364fa37bb7d9
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-02 22:10:12 -0400

    feat: implement project contestant count on project show page - User Story 3

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge = @project.challenge
+    @contestant_count = @project.count_contestants
   end
 end
