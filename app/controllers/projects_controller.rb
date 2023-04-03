6f2e20d091f097613fbb17db04526cb001345710
Grace Joh <graceehjoh@gmail.com>
2023-04-03 18:04:32 -0500

    feat: Add form to add contestant on Project show page (Extension 2)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,10 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def create
+    @project = Project.find(params[:id])
+    ContestantProject.create!(project_id: @project.id, contestant_id: params['contestant_id'])
+    redirect_to "/projects/#{@project.id}"
+  end
 end
