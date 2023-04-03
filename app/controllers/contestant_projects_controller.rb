f3abd338dd8af8615aa0917eb0cfd5ed7aed911d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-02 22:45:42 -0400

    feat: implement form to add a contestant to a specific project - User Story Extension 2

---
+++ app/controllers/contestant_projects_controller.rb
@@ -0,0 +1,6 @@
+class ContestantProjectsController < ApplicationController
+  def create
+    ContestantProject.create(contestant_id: params[:contestant_id], project_id: params[:project_id])
+    redirect_to "/projects/#{params[:project_id]}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/projects/:id', to: 'projects#show'
 
   get '/contestants', to: 'contestants#index'
+
+  post '/contestant_projects', to: 'contestant_projects#create'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
