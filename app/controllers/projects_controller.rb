37737f9fdace29bb5ffe4003dd6364e725c0c665
Thomas Hawley <thawley22@turing.edu>
2023-04-03 16:48:27 -0600

    feat: Add @sum_contestants variable to show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,5 +3,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge_theme = @project.challenge.theme
+    @sum_contestants = @project.num_of_contestants
   end
 end
