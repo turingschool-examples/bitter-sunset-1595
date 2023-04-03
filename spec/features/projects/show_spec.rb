81a52acc6e2ef8e10d1814989854450192923a81
Angel Byun <abyun22@turing.edu>
2023-04-03 16:43:40 -0600

    Add instance method and TDD in models for contestant_count

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,13 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Project Show" do
+  let!(:contestant_1) { Contestant.create!(name: "Kentaro Kameyama", age: 35, hometown: "Okinawa, Japan", years_of_experience: 25) }
+  let!(:contestant_2) { Contestant.create!(name: "Jay McCarroll", age: 21, hometown: "Los Angeles, California", years_of_experience: 3) }
+
   let!(:challenge_1) { Challenge.create!(theme: "Apartment Furnishing", project_budget: 1000)}
   let!(:challenge_2) { Challenge.create!(theme: "Party Clothes", project_budget: 500)}
   
   let!(:project_1) { Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge_1 ) }
   let!(:project_2) { Project.create!(name: "LeatherFeather", material: "Leather", challenge: challenge_2) }
   let!(:project_3) { Project.create!(name: "Rug Tuxedo", material: "Rug", challenge: challenge_1) }
-  
+
+  before do
+    ContestantProject.create!(contestant: contestant_1, project: project_1)
+    ContestantProject.create!(contestant: contestant_1, project: project_3)
+
+    ContestantProject.create!(contestant: contestant_2, project: project_2)
+  end
   describe "As a visitor, when I visit a '/projects/:id'" do
     it "shows projects name and material and theme of challenge that project belongs to" do
       visit "/projects/#{project_1.id}"
@@ -17,5 +26,11 @@ RSpec.describe "Project Show" do
       expect(page).to have_content(challenge_1.theme)
       expect(page).to have_content(challenge_1.project_budget)
     end
+
+    # it "shows a count of the number of contestants on this project" do
+    #   visit "/projects/#{project_1.id}"
+
+    #   expect()
+    # end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  let!(:contestant_1) { Contestant.create!(name: "Kentaro Kameyama", age: 35, hometown: "Okinawa, Japan", years_of_experience: 25) }
+  let!(:contestant_2) { Contestant.create!(name: "Jay McCarroll", age: 21, hometown: "Los Angeles, California", years_of_experience: 3) }
+
+  let!(:challenge_1) { Challenge.create!(theme: "Apartment Furnishing", project_budget: 1000)}
+  let!(:challenge_2) { Challenge.create!(theme: "Party Clothes", project_budget: 500)}
+  
+  let!(:project_1) { Project.create!(name: "Litfit", material: "Lamp Shade", challenge: challenge_1 ) }
+  let!(:project_2) { Project.create!(name: "LeatherFeather", material: "Leather", challenge: challenge_2) }
+  let!(:project_3) { Project.create!(name: "Rug Tuxedo", material: "Rug", challenge: challenge_1) }
+
+  before do
+    ContestantProject.create!(contestant: contestant_1, project: project_1)
+    ContestantProject.create!(contestant: contestant_1, project: project_3)
+
+    ContestantProject.create!(contestant: contestant_2, project: project_2)
+    ContestantProject.create!(contestant: contestant_2, project: project_1)
+  end
+
+  describe "instance methods" do
+    it "contestant_count" do
+      expect(project_1.contestant_count).to eq(2)
+      expect(project_2.contestant_count).to eq(1)
+      expect(project_3.contestant_count).to eq(1)
+    end
+  end
 end
