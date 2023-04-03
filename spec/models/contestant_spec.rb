15c3b3bc0439f3ecc23ad03b9d167a521a4a4b29
Thomas Hawley <thawley22@turing.edu>
2023-04-03 16:32:04 -0600

    feat: Add #all_projects instance method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe 'instance methods' do
+    let!(:recycled_material_challenge) { Challenge.create!(theme: "Recycled Material", project_budget: 1000) }
+    let!(:furniture_challenge) { Challenge.create!(theme: "Apartment Furnishings", project_budget: 1000) }
+
+    let!(:news_chic) { recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper") }
+    let!(:upholstery_tux) { furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch") }
+
+    let!(:gretchen) { Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) }
+
+    let!(:con_proj_2) { ContestantProject.create!(contestant_id: gretchen.id, project_id: news_chic.id) }
+    let!(:con_proj_3) { ContestantProject.create!(contestant_id: gretchen.id, project_id: upholstery_tux.id) }
+    it '#all_projects' do
+      expect(gretchen.all_projects).to eq(['News Chic', 'Upholstery Tuxedo'])
+    end
+  end
 end
