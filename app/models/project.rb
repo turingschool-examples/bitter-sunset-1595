2af091ce297bdefb608df8d62bf67dfeced04378
Grace Joh <graceehjoh@gmail.com>
2023-04-03 16:57:34 -0500

    feat: add Project #average_experience

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,10 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def average_experience
+    return 0 if contestants.empty?
+
+    contestants.average(:years_of_experience)
+  end
 end
