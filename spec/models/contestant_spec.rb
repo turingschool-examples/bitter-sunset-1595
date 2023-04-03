88ba3c05169efeef4102e3d29fd6a3cc7f0e487b
Nick Sacco <nicksacco10@gmail.com>
2023-04-03 15:42:32 -0700

    avg experience model test

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "class methods" do
+    it "::avg_experience" do
+    @challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+    @project = @challenge.projects.create!(name: "News Chic", material: "Newspaper")
+    
+    @contestant_1 = Contestant.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @contestant_2 = Contestant.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @contestant_3 = Contestant.create!(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @contestant_4 = Contestant.create!(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    @contestant_1.projects << @project
+    @contestant_2.projects << @project
+    @contestant_3.projects << @project
+    @contestant_4.projects << @project
+
+      expect(Contestant.avg_experience.round(2)).to eq(12)
+    end
+  end
 end
