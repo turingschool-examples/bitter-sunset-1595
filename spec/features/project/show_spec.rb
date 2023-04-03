9953aa2a995ffc3a701ea45e68fa9f760c17a900
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-04-02 19:15:28 -0600

    Test: Add tests for displaying number of contestants on the project show page

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -6,15 +6,22 @@ RSpec.describe 'Project Show Page', type: :feature do
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    # @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-    # @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    # @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
-    # @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    # @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    # @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-    # @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   # User Story 1
@@ -25,9 +32,16 @@ RSpec.describe 'Project Show Page', type: :feature do
   
   it 'has a name, material, and theme of the challenge that this project belongs to' do
     visit "/projects/#{@news_chic.id}"
-    save_and_open_page
+
     expect(page).to have_content(@news_chic.name)
     expect(page).to have_content(@news_chic.material)
     expect(page).to have_content(@recycled_material_challenge.theme)
+  end
+
+  # User Story 3
+  it 'has a count of the number of contestants on this project' do
+    visit "/projects/#{@news_chic.id}"
+    
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
