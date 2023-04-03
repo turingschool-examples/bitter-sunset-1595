65552768c5a7df778521b8a8a278888badbfbb8c
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-04-02 23:42:24 -0500

    Feat: add contestant to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    if params["Contestant id"]
+      ContestantProject.create(project_id: @project.id, contestant_id: params["Contestant id"])
+    end
   end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -19,4 +19,33 @@ RSpec.describe 'Projects Show Page' do
       expect(page).to have_content("Number of Contestants: #{project.contestant_count}")
     end
   end
+
+  Contestant.all.each_with_index do |contestant, i|
+    it 'has a form to add a contestant to the project' do
+      visit "/projects/#{@lit_fit.id}"
+
+      fill_in("Contestant id", with: contestant.id)
+      click_button("Add Contestant")
+
+      expect(current_path).to eq("/projects/#{@lit_fit.id}")
+      expect(page).to have_content("Number of Contestants: #{i + 1}")
+
+      value = @lit_fit.contestants.pluck(:years_of_experience).sum.fdiv(i + 1)
+      average_contestant_experience = value.nan? ? 0 : value
+
+      expect(page).to have_content("Average Contestant Experience: #{average_contestant_experience}")
+    end
+  end
+
+  it 'shows the project under the contestant\'s name after adding the contestant to the project' do
+    visit "/projects/#{@lit_fit.id}"
+
+    fill_in("Contestant id", with: @kentaro.id)
+    click_button("Add Contestant To Project")
+
+    visit "/contestants"
+
+    expect(page).to have_content(@lit_fit.name)
+    expect(@kentaro.name).to appear_before(@lit_fit.name)
+  end
 end
