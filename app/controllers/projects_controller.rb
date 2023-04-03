06a6009602274c58ca4bf781e36e11218854e3d9
Thomas Hawley <thawley22@turing.edu>
2023-04-03 17:18:25 -0600

    feat: Add @contestant_experience variable

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,6 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @challenge_theme = @project.challenge.theme
     @sum_contestants = @project.num_of_contestants
+    @contestant_experience = @project.avg_experience
   end
 end
