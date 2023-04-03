703d2758d362bbde0899cf929094b259c6f7a78a
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-02 22:00:28 -0500

    User story 1

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,19 +7,19 @@
 require 'rails_helper'
 
 RSpec.describe 'the Project show page' do
-  let!(recycled_material_challenge) { Challenge.create(theme: "Recycled Material", project_budget: 1000) }
-  let!(furniture_challenge) { Challenge.create(theme: "Apartment Furnishings", project_budget: 1000) }
+  let!(:recycled_material_challenge) { Challenge.create(theme: "Recycled Material", project_budget: 1000) }
+  let!(:furniture_challenge) { Challenge.create(theme: "Apartment Furnishings", project_budget: 1000) }
 
-  let!(news_chic) { recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper") }
-  let!(boardfit) { recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes") }
+  let!(:news_chic) { recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper") }
+  let!(:boardfit) { recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes") }
 
-  let!(upholstery_tux) { furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch") }
-  let!(lit_fit) { furniture_challenge.projects.create(name: "Litfit", material: "Lamp") }
+  let!(:upholstery_tux) { furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch") }
+  let!(:lit_fit) { furniture_challenge.projects.create(name: "Litfit", material: "Lamp") }
 
   describe 'displays the project attributes' do
     it "should display all of the project's attributes" do
-      visit "/project/#{news_chic.id}"
-
+      visit "/projects/#{news_chic.id}"
+      save_and_open_page
       expect(page).to have_content(news_chic.name)
       expect(page).to have_content(news_chic.material)
       expect(page).to have_content(news_chic.material)
