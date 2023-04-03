138a0537c84f7072087d6f01f72094d56afa657c
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-04-02 22:20:56 -0500

    Refactor: tests

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Contestants Index Page' do
-  before(:each) do
-    visit '/contestants'
-  end
+  Contestant.all.each do |contestant|
+    before(:each) do
+      visit '/contestants'
+    end
 
-  it 'shows all the contestant\'s names' do
-    expect(page).to have_content(@jay.name)
-    expect(page).to have_content(@gretchen.name)
-    expect(page).to have_content(@kentaro.name)
-    expect(page).to have_content(@erin.name)
-  end
+    it 'shows all the contestant\'s names' do
+      expect(page).to have_content(contestant.name)
+    end
 
-  it 'shows all the project names for each contestant' do
-    expect(page).to have_content("Projects: #{@jay.projects.pluck(:name).join(', ')}")
-    expect(page).to have_content("Projects: #{@gretchen.projects.pluck(:name).join(', ')}")
-    expect(page).to have_content("Projects: #{@kentaro.projects.pluck(:name).join(', ')}")
-    expect(page).to have_content("Projects: #{@erin.projects.pluck(:name).join(', ')}")
+    it 'shows all the project names for each contestant' do
+      expect(page).to have_content("Projects: #{contestant.projects.pluck(:name).join(', ')}")
+    end
   end
 end
