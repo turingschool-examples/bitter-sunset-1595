78e7e83bc73f8a8e8988884183698c153fb89309
Isaac Thill <ithill22@turing.edu>
2023-04-03 19:50:43 -0600

    test: add feature test for user story 3

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,6 +7,22 @@ RSpec.describe 'Project Show Page' do
 
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   describe 'As a visitor, when I visit /projects/:id' do
@@ -19,6 +35,20 @@ RSpec.describe 'Project Show Page' do
 
       expect(page).to_not have_content(@boardfit.name)
       expect(page).to_not have_content(@furniture_challenge.theme)
+    end
+
+    it 'I see the number of contestants on this project' do
+      visit "/projects/#{@news_chic.id}"
+      
+      expect(page).to have_content("Number of Contestants: 2")
+
+      visit "/projects/#{@boardfit.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
+
+      visit "/projects/#{@upholstery_tux.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
     end
   end
 end
