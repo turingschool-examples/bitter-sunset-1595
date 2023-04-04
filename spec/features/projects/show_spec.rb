0ea24567cc9217fc6e9c01bb4fe0754e88dc2830
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-04 14:56:32 -0600

    test: add test to show number of contestants on project show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe "Project's Show Page" do
 
     @upholstery_tux = @furniture_challenge.projects.create!(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create!(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   describe "When I visit a project's show page ('/projects/:id')" do 
@@ -26,5 +38,16 @@ RSpec.describe "Project's Show Page" do
       expect(page).to have_content("Material: Newspaper")
       expect(page).to have_content("Challenge Theme: Recycled Material")
     end
+
+    it 'Gives a count of all Contestants in that Project' do
+      visit "/projects/#{@news_chic.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
+
+      visit "/projects/#{@boardfit.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
+    end
   end
 end
+
