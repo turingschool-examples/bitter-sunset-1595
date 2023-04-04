609b0e854f434bbf7f84550490dd8c7e8f3283b8
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-04 14:30:31 -0600

    test: add model test for project_names instance method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    end
+
+    it '#project_names' do
+      expect(@gretchen.project_names).to eq("Upholstery Tuxedo, News Chic")
+    end
+  end
 end
