5d4501ac8d8a016b7675911b9232610b8026b3c8
Katie Lonsdale <katie952@gmail.com>
2023-04-04 13:42:13 -0600

    feat: project_names method

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,9 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def project_names
+    projects = ContestantProject.select(:project_id).where(contestant_id: id)
+    Project.where(id: projects).pluck(:name)
+  end
 end
