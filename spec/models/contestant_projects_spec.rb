c353428f9cf2b159c0f9db872adb2f0bfa45d96c
Katie Lonsdale <katie952@gmail.com>
2023-04-04 15:17:43 -0600

    test: validations

--- spec/models/contestant_projects_spec.rb
+++ spec/models/contestant_projects_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 
 RSpec.describe ContestantProject, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :project_id}
+    it {should validate_presence_of :contestant_id}
+  end
   describe "relationships" do
     it {should belong_to :contestant}
     it {should belong_to :project}
