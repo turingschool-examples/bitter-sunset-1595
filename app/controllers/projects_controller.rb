f7415f6658e4464974800ede6273d6b04d04eb56
Isaac Thill <ithill22@turing.edu>
2023-04-03 19:56:27 -0600

    feat: add controller logic and html for user story 3

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge = Challenge.find(@project.challenge_id)
+    @contestant_projects = ContestantProject.where(project_id: @project.id)
   end
 end
