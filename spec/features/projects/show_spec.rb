d9c1ae52e2aa3d22b695684e0fe58340616f1a86
Katie Lonsdale <katie952@gmail.com>
2023-04-04 15:10:04 -0600

    test: form to add existing contestant to project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -31,5 +31,26 @@ RSpec.describe 'projects show page' do
         expect(page).to have_content("Average Contestant Experience: 12.5 years")
       end
     end
+
+    it 'has a form to add an existing contestant to a project with their id' do
+      within('#add-contestant') do
+        expect(page).to have_content("Add a Contestant")
+        kentaro_id = @kentaro.id
+        fill_in(:contestant_id, with: kentaro_id)
+        click_button('Add Contestant To Project')
+      end
+
+      expect(current_path).to eq("/projects/#{@news_chic.id}")
+      
+      within('#project-info') do
+        expect(page).to have_content("Number of Contestants: 3")
+      end
+
+      visit '/contestants'
+
+      within("#contestant-#{@kentaro.id}") do
+        expect(page).to have_content("News Chic")
+      end
+    end
   end
 end
