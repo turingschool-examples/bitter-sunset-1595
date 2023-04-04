6cbbe2efe074bc9ed264b79b09240d273f36671c
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-04 16:58:34 -0400

    user story 3

--- spec/feature/projects/show_spec.rb
+++ spec/feature/projects/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'US 1: Project Show Page' do
+RSpec.describe 'Project Show Page' do
   before :each do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
@@ -12,10 +12,17 @@ RSpec.describe 'US 1: Project Show Page' do
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
   end
 
-  it 'can see the project name and material' do
+  it 'US 1: I can see the project name and material and theme' do
     visit "projects/#{@news_chic.id}"
 
     expect(page).to have_content(@news_chic.name)
     expect(page).to have_content(@news_chic.material)
+    expect(page).to have_content(@news_chic.challenge.theme)
+  end
+
+  it 'US 3: I can see the count of contestants on this project' do
+    visit "projects/#{@news_chic.id}"
+
+    expect(page).to have_content("Number of Contestants: #{@news_chic.contestants.count}")
   end
 end
