d61f9e01047200c3ee69e0dcd9165414c0053ed7
Katie Lonsdale <katie952@gmail.com>
2023-04-04 13:41:49 -0600

    test: for project_names method

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "#instance methods" do
+    before :each do
+      test_data
+    end
+
+    describe '#project_names' do
+      it 'returns the names of the projects that the contestant has worked on' do
+        expect(@jay.project_names.sort).to eq(['News Chic'])
+        expect(@gretchen.project_names.sort).to eq(['News Chic', 'Upholstery Tuxedo'])
+        expect(@kentaro.project_names.sort).to eq(['Boardfit', 'Upholstery Tuxedo'])
+      end
+    end
+  end
 end
