28b3bb4e91319d654e0d58750e833d0485c6db43
Katie Lonsdale <katie952@gmail.com>
2023-04-04 15:19:29 -0600

    remove unneccessary route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/projects/:project_id', to: 'projects#show'
-  post '/projects/project_id', to: 'projects#create'
 
   get '/contestants', to: 'contestants#index'
 
