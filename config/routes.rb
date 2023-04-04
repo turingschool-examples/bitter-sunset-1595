c825885f5906a363e7e11dcf85bdc1870f61149b
Katie Lonsdale <katie952@gmail.com>
2023-04-04 15:10:44 -0600

    add contestant_projects create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/projects/:project_id', to: 'projects#show'
+  post '/projects/project_id', to: 'projects#create'
 
   get '/contestants', to: 'contestants#index'
+
+  post '/contestant_projects/create', to: 'contestant_projects#create'
 end
